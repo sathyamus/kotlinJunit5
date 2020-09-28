593219511b007f56f0305852f104c4c6effde8d7
sathyamus <sathyamus.sify@gmail.com>
2020-09-29 00:12:40 +0530

    JUnit5 assertThrows, assertDoesNotThrow

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -3,6 +3,9 @@ class User(val id: Int, val name: String) {
         private set
 
     fun changeReputation(number: Int) {
+        if (number == 0)
+            throw Exception("Reputation change can't be ZERO")
         reputation+= number
     }
+
 }
--- src/test/kotlin/UserTest.kt
+++ src/test/kotlin/UserTest.kt
@@ -16,5 +16,20 @@ class UserTest {
         user.changeReputation(-5)
         Assertions.assertEquals(5, user.reputation)
     }
-    
+
+    @Test
+    fun `should throw exception when my reputation is ZERO`() {
+        Assertions.assertThrows(Exception::class.java) {
+            user.changeReputation(0)
+        }
+    }
+
+    @Test
+    fun `should not throw exception when my reputation is non ZERO`() {
+        user.changeReputation(5)
+        Assertions.assertDoesNotThrow { // Exception::class.java
+            println(this)
+        }
+    }
+
 }
