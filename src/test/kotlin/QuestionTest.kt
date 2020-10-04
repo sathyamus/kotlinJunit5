cb9176262c3aeb8c9610043663c493a2ff648d2b
sathyamus <sathyamus.sify@gmail.com>
2020-10-04 23:18:08 +0530

    (feat): Parameterized Test with Method Source

--- src/test/kotlin/QuestionTest.kt
+++ src/test/kotlin/QuestionTest.kt
@@ -2,7 +2,9 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
 
 class QuestionTest {
     var user = User(1, "Sathya")
@@ -23,6 +25,10 @@ class QuestionTest {
                 Question(1, "", "Sathya", "question 2")
             }
         }
+    }
+
+    @Nested
+    inner class `should constructor with Parameters` {
 
         @ParameterizedTest
         @CsvSource(
@@ -30,13 +36,30 @@ class QuestionTest {
                 "'', title",
                 "author, ' '",
                 "author, ''")
-        fun `throws an exception of title or author is invalid`(author: String, title: String) {
+        fun `throws an exception if title or author is invalid`(author: String, title: String) {
             Assertions.assertThrows(QuestionException::class.java) {
                 Question(1, author, title, "question")
             }
         }
 
+    }
 
+    companion object {
+        @JvmStatic
+        fun authorsAndTitles() = listOf(
+                Arguments.of("", "title"),
+                Arguments.of(" ", "title"),
+                Arguments.of("author", ""),
+                Arguments.of("author", " ")
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("authorsAndTitles")
+    fun `throws an exception if title or author is invalid`(author: String, title: String) {
+        Assertions.assertThrows(QuestionException::class.java) {
+            Question(1, author, title, "question")
+        }
     }
 
 }
